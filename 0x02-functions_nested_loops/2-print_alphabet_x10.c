957ed096676270bd5d94783e9f6f0f1c160014f0
Fuad Aminu Isa <aminufuad31@gmail.com>
2022-12-14 13:06:47 -0800

    nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,10 @@ void print_alphabet_x10(void)
 	char alphabet;
 	int j;
 
-	for (j = 0; j <= 10; j++)
+	for (j = 0; j < 10; j++)
 	{
 		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 			_putchar(alphabet);
-		_putchar(j);
+		_putchar('\n');
 	}
 }
