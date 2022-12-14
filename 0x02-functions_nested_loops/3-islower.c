b42793f5cc579c993c8a5ea1c98383f613dab4eb
Fuad Aminu Isa <aminufuad31@gmail.com>
2022-12-14 13:41:33 -0800

    nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,19 +2,17 @@
 
 /**
  *  _islower -  checks for lowercase character c
- *
+ *  @c: is an input char
  *  Return: 1 if lowercase and 0 if otherwise
- */
+*/
+
 int _islower(int c)
 {
 	char i;
 	int j;
 
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		if (i == c)
-			j = 1;
-	}
+	if (i == c)
+		j = 1;
 
 	return (j);
 }
