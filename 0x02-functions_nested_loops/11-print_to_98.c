1fba20cd72ae4da3eb920e8d31e4dc3710614bf1
Fuad Aminu Isa <aminufuad31@gmail.com>
2022-12-14 16:14:38 -0800

    nest

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (; n <= 98; n++)
+		for (n = n; n <= 98; n++)
 		{
 			printf("%d", n);
 
@@ -23,7 +23,7 @@ void print_to_98(int n)
 	}
 		else
 		{
-			for (; n >= 98; n--)
+			for (n = n; n >= 98; n--)
 			{
 				printf("%d", n);
 
