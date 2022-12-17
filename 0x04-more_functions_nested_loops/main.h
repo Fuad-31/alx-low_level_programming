f3cf60a813079cf052e965428700a0a9a2e3f6bd
Fuad Aminu Isa <aminufuad31@gmail.com>
2022-12-16 19:17:47 -0800

    nested loops

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H                                                                                                                                         
+#define MAIN_H                                                                                                                                         
+
+/**                                                                                                                                                    
+ * file: main.h                                                                                                                                        
+ * Athr: Fuad aminu Isah                                                                                                                               
+ * Desc: header file that contain all the functions and                                                                                                
+ * the prototype of the function _putchar                                                                                                              
+ */                                                                                                                                                    
+                                                                                                                                                     
+
+int _putchar(char c);                                                                                                                                  
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+#endif
