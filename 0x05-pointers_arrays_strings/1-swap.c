6b00c13d8c938202d1696195859d128a82856a58
Fuad Aminu Isa <aminufuad31@gmail.com>
2022-12-19 13:57:31 -0800

    Arrays, pointers and strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps the values of two integers
+ * @a: an inout integer pointer
+ * @b: an input integer pointer
+ * Return: always (0)
+ */
+
+void swap_int(int *a, int *b)
+
+{
+	int arr;
+	arr = *a;
+
+	*a = *b;
+	*b = arr;
+}
