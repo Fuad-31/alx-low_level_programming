e9f85abd8eb4bedb36130ce7692221cdc0d1841d
Fuad Aminu Isa <aminufuad31@gmail.com>
2022-12-20 05:40:32 -0800

    Pointers

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,9 +12,16 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
+	int j = 0;
 	
-	dest[i] = src[i];
-	i++;
-
+	while (*(src + 1) != '\0')
+	{
+		i++;
+	}
+	for (; j < i; j++)
+	{
+		dest[j] = src[j];
+	}
+	dest[i] = '\0';
 	return (dest);
 }
