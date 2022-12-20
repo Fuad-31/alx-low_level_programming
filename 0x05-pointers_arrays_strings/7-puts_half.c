494de9b44ba010c8a4b74de92fcffa4017dfb126
Fuad Aminu Isa <aminufuad31@gmail.com>
2022-12-19 21:08:37 -0800

    POinters, Strings and Arrays

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,11 +13,15 @@ void puts_half(char *str)
 
 	while (str[len] != '\0')
 		len++;
-		if (len % 2 == 0)
-			n = len / 2;
-		else
-			n = (len + 1) / 2;
-		for (i = n; i < len; i++)
-			_putchar(str[i]);
+
+	if (len % 2 == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
+
 	_putchar('\n');
 }
