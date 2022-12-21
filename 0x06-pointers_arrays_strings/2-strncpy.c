4e48de3225a76b668ee68433f59aa1c428f4a22f
Fuad Aminu Isa <aminufuad31@gmail.com>
2022-12-21 14:47:51 -0800

    POinters, Arrays and strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy - a function that copys the strings.
+ * @dest: an input string
+ * @src: an input string
+ * @n: an input integer
+ * Return: A pointer to the resulting string dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+	int i = 0;
+	int j = 0;
+	char *temp = dest, *start = src;
+
+	while (*src)
+	{
+		i++;
+		src++;
+	}
+	i++;
+	if (n > i)
+		n = i;
+	src = start;
+	for (; j < n; j++)
+		*dest++ = *src++;
+	return (temp);
+}
