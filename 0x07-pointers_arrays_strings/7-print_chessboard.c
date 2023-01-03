2fbf70b6fc086ac6c943e0edb8f00feb6a7c4265
Fuad Aminu Isa <aminufuad31@gmail.com>
2023-01-03 13:33:18 -0800

    more pointers

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,16 +6,13 @@
  * Return: Nothing
  */
 
-void print_chessboard(char (*a)[8])
-
+void print_chessboard(char (*a)[8]);
 {
-	int i, j;
-	
-	for (i = 0; i < 8; i++)
+	int i = 0, j;
+
+	for (; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-			_putchar(a[i][j]);
+				_putchar(a[i][j]);
 		_putchar('\n');
 	}
-
-}
