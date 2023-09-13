e340d6a60f66c848885783e4beace106fd5fcfaa
Fuad Aminu <aminufuad31@gmail.com>
2023-09-13 14:05:05 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - it returns 1 if value is small
+ * letter and 0 if otherwise
+ * @c: input variable
+ * Return: always 0
+ */
+
+int _islower(int c)
+{
+	if (_islower(c))
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+	return (0);
+}
