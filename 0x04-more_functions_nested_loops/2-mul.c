cc28fe5dc1ab660014e8755f4c8e594c28496730
Fuad Aminu <aminufuad31@gmail.com>
2023-09-14 06:48:49 -0700

    More loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies to integers
+ * @a: integer to be multiplied
+ * @b: integer to be multiplied
+ *
+ * Return: always j
+ */
+
+int mul(int a, int b)
+{
+	int j;
+
+	j = mul(a, b);
+	return (j);
+}
