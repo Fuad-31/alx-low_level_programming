8c777c035cb0cb39049529a46a7dec00469c4c35
Fuad Aminu <aminufuad31@gmail.com>
2023-09-25 16:41:41 -0700

    more pointers

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,34 +1,26 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 
 /**
  * main - Entry point
- * @n: input variable
  *
- * Description: 'The program is used to test the equality of n'
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	int n;
-	int m;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	m = n % 10;
 
-	if (m > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, m);
-	}
-	else if (m == 0)
-	{
-		printf("Last digit of %d is %d and is 0\n", n, m);
-	}
+	if (n % 10 > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	else if (n % 10 == 0)
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	else
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
-	}
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+
 	return (0);
 }
